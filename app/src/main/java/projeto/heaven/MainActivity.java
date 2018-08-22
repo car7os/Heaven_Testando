dbfda701143997da4e51b6134e425afdfcf527e4
root <carlosbezerravilela@gmail.com>
2018-08-22 20:16:11 -0300

    Classe Fala

--- app/src/main/java/projeto/heaven/MainActivity.java
+++ app/src/main/java/projeto/heaven/MainActivity.java
@@ -3,11 +3,16 @@ package projeto.heaven;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import projeto.heaven.audio.Falar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Falar out = new Falar(getApplicationContext());
+        out.falar("Olá Mundo!");
     }
 }
---
+++ app/src/main/java/projeto/heaven/audio/Falar.java
@@ -0,0 +1,41 @@
+package projeto.heaven.audio;
+
+import android.content.Context;
+
+import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class Falar {
+
+    protected TextToSpeech tts;
+    private Context contexto;
+
+    public Falar(Context contexto){
+        this.contexto = contexto;
+    }
+
+    public void falar(final String texto){
+
+
+        tts = new TextToSpeech(contexto, new TextToSpeech.OnInitListener() {
+
+
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int res = tts.setLanguage(new Locale("pt", "BR"));
+                    if (res >= TextToSpeech.LANG_AVAILABLE) {
+                        Toast.makeText(contexto, texto, Toast.LENGTH_SHORT).show();
+                        tts.speak(texto, TextToSpeech.QUEUE_FLUSH, null, null);
+
+                    }
+                }
+            }
+
+        });
+
+    }
+
+}
+
